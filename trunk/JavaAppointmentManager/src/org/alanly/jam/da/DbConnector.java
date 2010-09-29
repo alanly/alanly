6e74d2a995612163b3690f24e1ea52021be602e2
me@alanly.org <me@alanly.org@32c88330-f220-11dd-87c9-111d11f6bf44>
2010-09-29 17:20:58 +0000

    [JavaAppointmentManager] - Added MySQL connector - Created DbConnector abstract class

---
+++ trunk/JavaAppointmentManager/src/org/alanly/jam/da/DbConnector.java
@@ -0,0 +1,33 @@
+/**
+ * Contains the back-end logic required to connect to and perform operations on a database for the Java Appointment Manager.
+ */
+package org.alanly.jam.da;
+
+/**
+ * The <code>DbConnector</code> is responsible for handling database connections and operations.
+ * 
+ * @author Alan Ly
+ * @version 1.0
+ */
+public abstract class DbConnector {
+	
+	/**
+	 * Database connector details
+	 */
+	private final String DBMS_CONNECTOR = "jdbc";
+	private final String DBMS_TYPE = "mysql";
+	
+	/**
+	 * Database connection details
+	 */
+	private final String DB_HOST = "waldo.dawsoncollege.qc.ca";
+	private final String DB_PORT = "3306";
+	private final String DB_NAME = "";
+	private final String DB_USER = "A830207";
+	private final String DB_PASS = "";
+	
+	public DbConnector() {
+		super();
+	}
+	
+}
