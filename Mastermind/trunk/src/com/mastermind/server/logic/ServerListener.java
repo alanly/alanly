ca9273fbe5033fca24869de6239e4413c623894c
me@alanly.org <me@alanly.org@32c88330-f220-11dd-87c9-111d11f6bf44>
2010-10-17 02:44:57 +0000

    [Mastermind] - More or less finalised preliminary version of ServerListener class. - Created initial implementation of ServerThread, ready to accept GameLogic class. - Created ConsoleUtilities static class to hold utilities related to server-side console output.

--- Mastermind/trunk/src/com/mastermind/server/logic/ServerListener.java
+++ Mastermind/trunk/src/com/mastermind/server/logic/ServerListener.java
@@ -3,10 +3,109 @@
  */
 package com.mastermind.server.logic;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import com.mastermind.server.util.ConsoleUtilities;
+
 /**
+ * The <code>ServerListener</code> class listens for and handles client connections to the Mastermind server. 
  * @author Alan Ly
- *
+ * @version 1.0
  */
 public class ServerListener {
-
+    
+    /**
+     * Specifies the default listening port as 30,000.
+     */
+    private static final int DEFAULT_LISTENING_PORT = 30000;
+    
+    private int listeningPort;
+    private boolean listenForConnection;
+    
+    /**
+     * Create a <code>ServerListener</code> with the default listening port to listen for and handle client connections to the Mastermind server.
+     */
+    public ServerListener() {
+	this(DEFAULT_LISTENING_PORT);
+    }
+    
+    /**
+     * Create a <code>ServerListener</code> with a specified listening port to listen for and handle client connections to the Mastermind server.
+     * @param listeningPort the port on which to listen to for client connections
+     */
+    public ServerListener(int listeningPort) {
+	super();
+	this.listeningPort = listeningPort;
+    }
+    
+    /**
+     * Gets the current listening port number of the server.
+     * @return the port number to listen on
+     */
+    public int getListeningPort() {
+	return this.listeningPort;
+    }
+    
+    /**
+     * Sets the current listening port number of the server.
+     * @param listeningPort the port number to listen on
+     */
+    public void setListeningPort(int listeningPort) {
+	this.listeningPort = listeningPort;
+    }
+    
+    /**
+     * Starts listening for connections from clients on the appropriate listening port.
+     * 
+     * When a client connects, a <code>Socket</code> reference is created for the client and a new <code>ServerThread</code> is created to handle the client.
+     * @throws IOException thrown when a socket is unable to be opened
+     */
+    public void startListening() throws IOException {
+	ServerSocket serverSocket = null;
+	Socket clientSocket = null;
+	
+	// Opens a ServerSocket on the port specified by the 'listeningPort' variable
+	try {
+	    serverSocket = new ServerSocket(this.listeningPort);
+	    
+	    // Enable server to listen for connection
+	    this.listenForConnection = true;
+	    
+	    // Print out listening header for status
+	    System.out.println("[" + ConsoleUtilities.generateTimeStamp() + "] Server listening on port " + this.listeningPort + "...");
+	    
+	    // Run loop while condition is true to listen for clients
+	    while(this.listenForConnection) {
+		// Get client connection
+		clientSocket = serverSocket.accept();
+		
+		// Print status message for client connection
+		System.out.println("[" + ConsoleUtilities.generateTimeStamp() + "] Client connection from " + clientSocket.getInetAddress().getHostName());
+		
+		// Create a ServerThread
+		ServerThread serverThread = new ServerThread(clientSocket);
+		
+		// Start handling client
+		serverThread.startThread();
+	    }
+	} catch(IOException ioe) {
+	    // Throw new IOException with the appropriate message
+	    throw new IOException("Unable to open server socket on port " + this.listeningPort);
+	} finally {
+	    // Close the client socket if necessary
+	    if(clientSocket.isConnected())
+		clientSocket.close();
+	    
+	    // Close the server socket if necessary
+	    if(!serverSocket.isClosed())
+		serverSocket.close();
+	}
+    }
+    
+    public void stopListening() {
+	this.listenForConnection = false;
+    }
+    
 }
--- Mastermind/trunk/src/com/mastermind/server/logic/ServerThread.java
+++ Mastermind/trunk/src/com/mastermind/server/logic/ServerThread.java
@@ -3,12 +3,70 @@
  */
 package com.mastermind.server.logic;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import com.mastermind.server.util.ConsoleUtilities;
+
 /**
  * @author Alan Ly
- *
+ * @version 1.0
  */
 public class ServerThread {
-
     
+    /**
+     * Set the size of the message buffer for the byte array that holds messages sent back and fourth
+     */
+    private static final int MESSAGE_BUFFER_SIZE = 4;
+    
+    private Socket clientSocket;
+    private byte[] messageBuffer;
+    private InputStream inStream;
+    private OutputStream outStream;
+    
+    public ServerThread(Socket clientSocket) throws IOException {
+	super();
+	this.clientSocket = clientSocket;
+	
+	// Initialise message buffer
+	this.messageBuffer = new byte[MESSAGE_BUFFER_SIZE];
+	
+	// Instantiate IO streams
+	try {
+	    this.inStream = this.clientSocket.getInputStream();
+	    this.outStream = this.clientSocket.getOutputStream();
+	} catch (IOException ioe) {
+	    throw new IOException("Unable to retrieve IO stream from client socket");
+	}
+    }
 
+    public Socket getClientSocket() {
+	return this.clientSocket;
+    }
+    
+    public void setClientSocket(Socket clientSocket) {
+	this.clientSocket = clientSocket;
+    }
+    
+    public void startThread() throws IOException {
+	int receiveSize = 0;
+	
+	System.out.println("[" + ConsoleUtilities.generateTimeStamp() + "] Handling client from " + this.clientSocket.getInetAddress().getHostName());
+	
+	try {
+	    while((receiveSize = inStream.read(messageBuffer)) != -1) {
+	        
+	    }
+	} catch (IOException ioe) {
+	    throw new IOException("Unable to open IO stream");
+	} finally {
+	    if(this.clientSocket.isConnected())
+		this.clientSocket.close();
+	}
+	
+	System.out.println("[" + ConsoleUtilities.generateTimeStamp() + "] Client from " + this.clientSocket.getInetAddress().getHostName() + " disconnected");
+    }
+    
 }
