531839eba8e658306a5ac6680e9223e916276dcb
me@alanly.org <me@alanly.org@32c88330-f220-11dd-87c9-111d11f6bf44>
2010-10-17 02:54:18 +0000

    [Mastermind] - Created the ServerApplication class to launch the server.

--- Mastermind/trunk/src/com/mastermind/server/ServerApplication.java
+++ Mastermind/trunk/src/com/mastermind/server/ServerApplication.java
@@ -3,9 +3,13 @@
  */
 package com.mastermind.server;
 
+import java.io.IOException;
+
+import com.mastermind.server.logic.ServerListener;
+
 /**
  * @author Alan Ly
- *
+ * @version 1.0
  */
 public class ServerApplication {
 
@@ -13,8 +17,17 @@ public class ServerApplication {
      * @param args
      */
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
-
+	try {
+	    int serverPort = (args.length != 1 ? 30000 : Integer.parseInt(args[0]));
+	    
+	    ServerListener serverListener = new ServerListener(serverPort);
+	    
+	    serverListener.startListening();
+	} catch (NumberFormatException nfe) {
+	    System.err.println("Please provide a valid port number!");
+	} catch (IOException ioe) {
+	    System.err.println(ioe.getMessage());
+	}
     }
 
 }
